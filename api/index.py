b1f4b2231dcdb9e879f9fb4f514495e59e5676f8
Peggy2344 <peggy102104@gmail.com>
2023-04-23 01:22:17 +0800

    fix: 修改寫法

--- api/index.py
+++ api/index.py
@@ -12,12 +12,10 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 openai_key = OPENAI_API_KEY
 openai.api_key = openai_key
